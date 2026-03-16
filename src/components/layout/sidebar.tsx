daf3c48066a175cd1fbf9cee53c4bdf78f09891a
Chris Voss <christomdf@gmail.com>
2026-03-16 18:12:11 +0000

    Cómo corregirlo (La solución definitiva) La mayoría de los componentes d

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -57,15 +57,16 @@ const AppSidebar = () => {
         <SidebarMenu>
           {menuItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href}>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={{ children: item.label, side: 'right' }}
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                tooltip={{ children: item.label, side: 'right' }}
+              >
+                <Link href={item.href}>
                   <item.icon />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
